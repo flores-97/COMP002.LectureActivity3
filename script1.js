57a66cb35a2f16b2b26174a28b87130974e59a9a
flores-97 <floresvictor15@gmail.com>
2025-10-22 20:45:53 -0700

    steps 1-2 done

--- script1.js
+++ script1.js
@@ -1,23 +1,24 @@
 let myGrade;
-myGrade = "7"
+myGrade = "70";
 
 if(myGrade >=90)
 {
-    console.log("A")
+    console.log("A");
 }
 else if(myGrade <90 && myGrade >=80)
 {
-    console.log("B")
+    console.log("B");
 }
 else if(myGrade <80 && myGrade >=70)
 {
-    console.log("C")
+    console.log("C");
 }
 else if(myGrade <70 && myGrade >=60)
 {
-    console.log("D")
+    console.log("D");
 }
-else(myGrade <=59) //this area is what should be in the else statement because it closes the loop
+else if(myGrade <=59) 
 {
-    console.log("F")
+    console.log("F");
 }
+else(myGrade)//this area is what should be in the else statement because it is the default if previous conditions are met
